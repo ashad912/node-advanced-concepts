82bd93d29c32f50cd93463fc1fc6bbb5a81dcdbb
ashad912 <ashad912@gmail.com>
2020-06-03 17:40:36 +0200

    section-02 final

--- section-02/index.js
+++ section-02/index.js
@@ -1,37 +1,26 @@
 const express = require('express')
 const app = express()
-const cluster = require('cluster')
 const crypto = require('crypto')
 
-if(cluster.isMaster){
-    // Cause index.js to be executed *again* but
-    // in child mode
-    cluster.fork()
-    cluster.fork() //additional child
-}else{
-
-    // Im a child, Im going to act like a server
-    // and do nothing more
-
-    // function doWork(duration){
-    //     const start = Date.now()
-    //     while(Date.now() - start < duration){}
-    // }
-
-    app.get('/', (req, res) => {
-        //doWork(5000)
-        crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
-            console.log('Hash:', Date.now() - start, 'ms')
-            res.send('Hi there!')
-        })
-        
+app.get('/', (req, res) => {
+    //doWork(5000)
+    crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
+        console.log('Hash:', Date.now() - start, 'ms')
+        res.send('Hi there!')
     })
+    
+})
 
-    app.get('/fast', (req, res) => {
-        res.send('This was fast')  
-    })
+app.get('/fast', (req, res) => {
+    res.send('This was fast')  
+})
 
-    app.listen(3000)
-}
+app.listen(3000)
 
-//nodemon is not recommended for cluster mode!!!
+//pm2 start index.js -i 0
+//0 - pm2 figures out, how many instance to create
+
+//pm2 list
+//pm2 show index
+//pm2 monit
+//pm2 delete index
