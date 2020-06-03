411835cc85ad0a3fdcd622f86fa01a2c9fda6229
ashad912 <ashad912@gmail.com>
2020-06-03 02:50:58 +0200

    clusters

--- section-02/index.js
+++ section-02/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cluster = require('cluster')
+const crypto = require('crypto')
 
 if(cluster.isMaster){
     // Cause index.js to be executed *again* but
@@ -12,14 +13,18 @@ if(cluster.isMaster){
     // Im a child, Im going to act like a server
     // and do nothing more
 
-    function doWork(duration){
-        const start = Date.now()
-        while(Date.now() - start < duration){}
-    }
+    // function doWork(duration){
+    //     const start = Date.now()
+    //     while(Date.now() - start < duration){}
+    // }
 
     app.get('/', (req, res) => {
-        doWork(5000)
-        res.send('Hi there!')
+        //doWork(5000)
+        crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
+            console.log('Hash:', Date.now() - start, 'ms')
+            res.send('Hi there!')
+        })
+        
     })
 
     app.get('/fast', (req, res) => {
